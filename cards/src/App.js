7347649038103ed2c9aa38b31faab9089da8f31a
Patrick Lewis <lewispjjpb@gmail.com>
2022-01-23 14:00:28 -0500

    Add condition filter dropdown

--- cards/src/App.js
+++ cards/src/App.js
@@ -13,7 +13,8 @@ class App extends React.Component  {
       filterPatient: '',
       patientIsFiltered: false,
       specificCondition: '',
-      conditionIsFiltered: false
+      conditionIsFiltered: false,
+      allArrhythmias: [],
     };
 
     this.changeStatus = this.changeStatus.bind(this);
@@ -36,14 +37,24 @@ class App extends React.Component  {
         for (let i = 0; i < results.data.length; i++) {
           let status = results.data[i].status;
           let conditions = [];
-          for (let j = 0; j < results.data[i][j].arrhythmias) {
-            conditions.push()
+          for (let j = 0; j < results.data[i].arrhythmias.length; j++) {
+            conditions.push(results.data[i].arrhythmias[j])
           }
           update[status].push(results.data[i])
         }
         this.setState({PENDING: update.PENDING})
         this.setState({REJECTED: update.REJECTED})
         this.setState({DONE: update.DONE})
+        let allCards = this.state.PENDING.concat(this.state.DONE, this.state.REJECTED)
+        let allSymptoms = new Set();
+        for (let i = 0; i < allCards.length; i++) {
+          let thisCard = allCards[i];
+          for (let j = 0; j < thisCard.arrhythmias.length; j++) {
+            allSymptoms.add(thisCard.arrhythmias[j])
+          }
+        }
+        allSymptoms = Array.from(allSymptoms)
+        this.setState({'allArrhythmias': allSymptoms})
         })
       .catch(err => console.log(err))
   }
@@ -88,6 +99,14 @@ class App extends React.Component  {
 
   render() {
     let allCards = this.state.PENDING.concat(this.state.DONE, this.state.REJECTED)
+    // let allSymptoms = new Set();
+    // for (let i = 0; i < allCards.length; i++) {
+    //   let thisCard = allCards[i];
+    //   for (let j = 0; j < thisCard.arrhythmias.length; j++) {
+    //     allSymptoms.add(thisCard.arrhythmias[j])
+    //   }
+    // }
+    // this.setState({allArrhythmias: allSymptoms})
     return (
       <div className="App">
         <header className="App-header">Card triage</header>
@@ -102,7 +121,7 @@ class App extends React.Component  {
           <div>
             Select arrhythmias:
             <select value={this.state.specificCondition} onChange={this.filterConditions}>
-              {allCards.map(card => <option value={card.arrhythmias}>{card.arrhythmias}</option>)}
+              {this.state.allArrhythmias.map(card => <option value={card}>{card}</option>)}
             </select>
           </div>
           <button onClick={() => this.setState({conditionIsFiltered: false})}>Clear condition filter</button>
@@ -110,33 +129,57 @@ class App extends React.Component  {
         <div className="Cards">
           <h5>
             Pending: {this.state.PENDING.map(card => {
-              if (!this.state.patientIsFiltered) {
+              if (!this.state.patientIsFiltered && !this.state.conditionIsFiltered) {
                 return <Card data={card} updateStatus={this.changeStatus} stat={card.status}/>
-              } else {
+              } else if (!this.state.conditionIsFiltered && this.state.patientIsFiltered) {
                 if (card.patient_name === this.state.filterPatient) {
                  return <Card data={card} updateStatus={this.changeStatus} stat={card.status}/>
+                }
+              } else if (this.state.conditionIsFiltered && !this.state.patientIsFiltered) {
+                if (card.arrhythmias.indexOf(this.state.specificCondition) !== -1) {
+                  return <Card data={card} updateStatus={this.changeStatus} stat={card.status}/>
+                 }
+              } else {
+                if (card.arrhythmias.indexOf(this.state.specificCondition) !== -1 && card.patient_name === this.state.filterPatient) {
+                  return <Card data={card} updateStatus={this.changeStatus} stat={card.status}/>
                 }
               }
             })}
           </h5>
           <h5>
             Done: {this.state.DONE.map(card => {
-              if (!this.state.patientIsFiltered) {
+              if (!this.state.patientIsFiltered && !this.state.conditionIsFiltered) {
                 return <Card data={card} updateStatus={this.changeStatus} stat={card.status}/>
-              } else {
+              } else if (!this.state.conditionIsFiltered && this.state.patientIsFiltered) {
                 if (card.patient_name === this.state.filterPatient) {
                  return <Card data={card} updateStatus={this.changeStatus} stat={card.status}/>
+                }
+              } else if (this.state.conditionIsFiltered && !this.state.patientIsFiltered) {
+                if (card.arrhythmias.indexOf(this.state.specificCondition) !== -1) {
+                  return <Card data={card} updateStatus={this.changeStatus} stat={card.status}/>
+                 }
+              } else {
+                if (card.arrhythmias.indexOf(this.state.specificCondition) !== -1 && card.patient_name === this.state.filterPatient) {
+                  return <Card data={card} updateStatus={this.changeStatus} stat={card.status}/>
                 }
               }
             })}
           </h5>
           <h5>
             Rejected: {this.state.REJECTED.map(card => {
-              if (!this.state.patientIsFiltered) {
+              if (!this.state.patientIsFiltered && !this.state.conditionIsFiltered) {
                 return <Card data={card} updateStatus={this.changeStatus} stat={card.status}/>
-              } else {
+              } else if (!this.state.conditionIsFiltered && this.state.patientIsFiltered) {
                 if (card.patient_name === this.state.filterPatient) {
                  return <Card data={card} updateStatus={this.changeStatus} stat={card.status}/>
+                }
+              } else if (this.state.conditionIsFiltered && !this.state.patientIsFiltered) {
+                if (card.arrhythmias.indexOf(this.state.specificCondition) !== -1) {
+                  return <Card data={card} updateStatus={this.changeStatus} stat={card.status}/>
+                 }
+              } else {
+                if (card.arrhythmias.indexOf(this.state.specificCondition) !== -1 && card.patient_name === this.state.filterPatient) {
+                  return <Card data={card} updateStatus={this.changeStatus} stat={card.status}/>
                 }
               }
             })}
